da215e2a73ea0cd5cb652a9305acb8ec6d092cc9
Alekzandern <130380585+Alekzandern@users.noreply.github.com>
2023-11-09 22:21:57 +0700

    Update SQLHelper.java

--- src/test/java/data/SQLHelper.java
+++ src/test/java/data/SQLHelper.java
@@ -13,7 +13,7 @@ public class SQLHelper {
 
     }
 
-    private static final String DB_URL = System.getProperty("db.url");
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/app";
 
     @SneakyThrows
     public static Connection getConnection() {
